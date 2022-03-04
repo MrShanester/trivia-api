cd5499d36741c46210c1c22a1f365ea7e197427d
Shane Nicholson <shanescottnicholson@gmail.com>
2022-03-04 11:05:48 -0800

    Fixes variable name in options routes and adds routes for questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/options" => "products#index"
-  get "/options/:id" => "products#show"
-  post "/options" => "products#create"
-  patch "/options/:id" => "products#update"
-  delete "/options/:id" => "products#destroy"
+  # Options controller
+  get "/options" => "options#index"
+  get "/options/:id" => "options#show"
+  post "/options" => "options#create"
+  patch "/options/:id" => "options#update"
+  delete "/options/:id" => "options#destroy"
+
+  # Questions Controller
+  get "/questions" => "questions#index"
+  get "/questions/:id" => "questions#show"
+  post "/questions" => "questions#create"
+  patch "/questions/:id" => "questions#update"
+  delete "/questions/:id" => "questions#destroy"
 end
