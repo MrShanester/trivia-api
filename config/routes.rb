1c7b78ec470f0fce7a2892454b1e14a7e385aae8
Shane Nicholson <shanescottnicholson@gmail.com>
2022-03-04 10:53:46 -0800

    Adds routes for options

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/options" => "products#index"
+  get "/options/:id" => "products#show"
+  post "/options" => "products#create"
+  patch "/options/:id" => "products#update"
+  delete "/options/:id" => "products#destroy"
 end
