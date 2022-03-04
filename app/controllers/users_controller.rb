c84cad78a1963be3b64b581697332a53481d2252
Shane Nicholson <shanescottnicholson@gmail.com>
2022-03-04 11:36:51 -0800

    Adds REST methods to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  def index
+    output = User.all
+    render json: output.as_json
+  end
+
+  def show
+    output = User.find_by(id: params[id:])
+    render json: output.as_json
+  end
+
+  def create
+    output = User.new(username: params[username:], points: 0)
+    if output.save
+      render json: {message: "User created successfully"}
+    else
+      render json: {message: "U Failure"}
+    end
+  end
+
+  def update
+    output = User.find_by id: params[id:]
+    output.username = params[username:] || output.username
+    output.points = output.points
+    if output.save
+      render json: {message: "User updated successfully"}
+    else
+      render json: {message: "U Failure"}
+    end
+  end
+
+  def destroy
+    output = User.find_by(id: params[id:])
+    output.delete
+    render json: {message: "L + Ratio"}
+  end
 end
