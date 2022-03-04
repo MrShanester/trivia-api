87c00ae25e2b99bcfcdc7c944cc194c341e35bf1
Shane Nicholson <shanescottnicholson@gmail.com>
2022-03-04 11:43:21 -0800

    Adds and runs user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 # Question.create!([{question: "Who is the best friend of all the friends?", answer: "Shane!"}, {question: "Who made this website?", answer: "Shane!"}, {question: "Who has a beautiful golden mane?", answer: "Shane!"}, {question: "Who is increasingly coming off as an extreme narcissist?", answer: "Shane!"}])
 
 # Option.create!([{question_id: 1, option: "Shane!"}, {question_id: 1, option: "Tanner!"}, {question_id: 1, option: "Alex!"}, {question_id: 1, option: "Ome!"}, {question_id: 2, option: "Shane!"}, {question_id: 2, option: "Tanner!"}, {question_id: 2, option: "Alex!"}, {question_id: 2, option: "Ome!"}, {question_id: 3, option: "Shane!"}, {question_id: 3, option: "Tanner!"}, {question_id: 3, option: "Alex!"}, {question_id: 3, option: "Ome!"}, {question_id: 4, option: "Shane!"}, {question_id: 4, option: "Tanner!"}, {question_id: 4, option: "Alex!"}, {question_id: 4, option: "Ome!"}])
+
+# User.create!({username: "MrShanester"})
